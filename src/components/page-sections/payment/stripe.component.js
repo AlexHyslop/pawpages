81b3e8a99279f9a0b2b6452a21a1afe7d0003efb
Dylan Stables <dylan5142.stables@gmail.com>
2024-05-09 23:28:03 +0100

    stripe start

---
+++ src/components/page-sections/payment/stripe.component.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import {
+  CardElement,
+  Elements,
+  useStripe,
+  useElements,
+} from '@stripe/react-stripe-js';
+
+export default function Stripe(props) { 
+
+const stripePromise = loadStripe('stripekey');
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+    });
+
+    if (!error) {
+      const { id } = paymentMethod;
+      // send id to server, create PaymentIntent and confirm payment
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement />
+      <button type="submit" disabled={!stripe}>
+        Pay
+      </button>
+    </form>
+  );
+};
+
+ 
