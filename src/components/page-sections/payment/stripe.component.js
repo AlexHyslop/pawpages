165ccd7f6c63c4f0861e8e92eaf758f15a129eb1
Dylan Stables <dylan5142.stables@gmail.com>
2024-07-22 23:28:36 +0100

    api working

--- src/components/page-sections/payment/stripe.component.js
+++ src/components/page-sections/payment/stripe.component.js
@@ -110,13 +110,11 @@ function CheckoutForm() {
           console.log("Selected service", selectedService);
 
           var collectionService = selectedService.CollectionOptions.filter(res => res.CollectionOptionTitle === "DHLParcel");
-          selectedService = selectedService[0];
-
           if(collectionService.length == 0){
-            collectionService = selectedService.s.filter(res => res.CollectionOptionTitle === "DHLParcelMulti");
+            collectionService = selectedService.CollectionOptions.filter(res => res.CollectionOptionTitle === "DHLParcelMulti");
           }
-          
           collectionService = collectionService[0];
+
           console.log("Collection option", collectionService);
 
           var shipmentObject = {
